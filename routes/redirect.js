2801b7af6b25e793ed93ff5c4ff558a5fa6d6a7d
saitulasi <palchurisaitulasi780@gmail.com>
2025-11-23 14:59:19 +0530

    Fix redirect route regex

--- routes/redirect.js
+++ routes/redirect.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { getLongUrl, incrementClick } = require("../db/index");
 
+const { getLongUrl, updateStats } = require("../db/index");
+
+// REDIRECT ROUTE (/:code)
 router.get("/:code([A-Za-z0-9]{5,8})", (req, res) => {
   const code = req.params.code;
+
   const longUrl = getLongUrl(code);
 
   if (!longUrl) {
     return res.status(404).send("Short URL not found");
   }
 
-  // Increment clicks + update lastClicked
-  incrementClick(code);
+  // update clicks + last clicked
+  updateStats(code);
 
   return res.redirect(longUrl);
 });
